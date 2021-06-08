2498f1d49e61cc61521c90b081069e2671a17f3c
Ram <55042314+bachinaram@users.noreply.github.com>
2021-06-08 09:04:31 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,16 @@
+#signature
+#single line comments using #
+'''
+triple single quotes
+'''
+
+'''
+Program - comments.py
+developer/s - institute
+date - 10/06/2021
+team - RedRangers
+usage - addition program
+'''
+
+
+
---
+++ print.py
@@ -0,0 +1,29 @@
+#learn print
+name1="student1"
+name2="student2"
+name3="student3"
+..
+..
+
+print("hello students with double quotes")
+print('hello students single quotes')
+print("hello","gaurav","sai")
+print('hello','gaurav','sai')
+print("hello"+"gaurav"+"sai")
+print(name1, name2)
+print("hello",name1 +" and", name2)
+print("hello "+ name1 +" and "+ name2)
+print("{} {}".format(name1,name2))
+print("{0} {1}".format(name1,name2))
+print("{1} {0}".format(name1,name2))
+print("{1} {0} {2}".format(name1,name2,name3))
+print("hello","gaurav","sai",sep="!")
+print("student1","gaurav","sai",sep=",")
+print("first line",end="+")
+print("last line")
+print("second first line",end=" ")
+print("seconnd last line", end="+")
+print('third line')
+
+print("hello!","gaurav","sai",sep=",",end=" ")
+print("how are you doing")
