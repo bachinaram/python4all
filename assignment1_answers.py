49b5b04c06de56bfa988fa689ab6c8d06044a561
Ram <55042314+bachinaram@users.noreply.github.com>
2021-06-09 09:33:01 +0530

    Add files via upload
    assignment2 questions and assignment1 answers

---
+++ assignment1_answers.py
@@ -0,0 +1,54 @@
+# Homework1
+# print output in below format with single line of code
+# classes:
+#         class1
+#         class2
+#         class3
+print("classes:\n\tclass1\n\tclass2\n\tclass3")
+# classes:
+#         class1
+#               PYTHON CLASS
+#         class2
+#                JAVA CLASS
+#         class3
+#                C CLASS
+print("classes:\n\tclass1\n\t\tPYTHON CLASS\n\tclass2\n\t\tJAVA CLASS\n\tclass3\n\t\tC CLASS")
+
+
+#print assignment - 1,2
+#use print function to print below output
+#assignment1
+
+#********************************
+#*********Welcome class**********
+#________________________________
+#**This is your first assigment**
+#________________________________
+#********************************
+print("********************************")
+print("*********Welcome class**********")
+print("________________________________")
+print("**This is your first assigment**")
+print("________________________________")
+print("********************************")
+
+
+#assignment2
+
+#********************************
+#________________________________
+#Program     - program_file_name
+#Developer   - xyz
+#Date        - DD/MM/YYYY
+#Usage       - Write about something
+#________________________________
+#********************************
+
+print("********************************")
+print("________________________________")
+print("Program     - program_file_name")
+print("Developer   - xyz")
+print("Date        - DD/MM/YYYY")
+print("Usage       - Write about something")
+print("________________________________")
+print("********************************")
