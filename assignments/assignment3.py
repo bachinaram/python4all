23e1ba1a80ec56434a98b75aae7fa7a6a3d29293
Ram <55042314+bachinaram@users.noreply.github.com>
2021-06-10 17:57:40 +0530

    assignment3 on input, type conver and variables

---
+++ assignments/assignment3.py
@@ -0,0 +1,17 @@
+print("***************************************************")
+print("Now program is going to take two user inputs")
+print("____________________________________")
+my_city=input("Enter your city: ")
+my_state=input("Enter your State: ")
+print("***************************************************")
+my_details=my_city+","+my_state
+print(my_details)
+print("***************************************************")
+
+print("FIRST PROGRAM COMPLETED")
+print("RUNNING SECOND PROGRAM")
+
+num1=int(input("Enter Number1:"))
+num2=int(input("Enter Number2:"))
+float_num=float(input("Enter Float Number:"))
+print("Output is:",num1+num2+float_num)
